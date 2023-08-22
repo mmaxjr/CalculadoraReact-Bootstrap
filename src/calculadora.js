a9f678d198286abbe20d73f1c6fb8d7449fd1f4f
mmaxjr <mmaxmjunior@gmail.com>
2023-08-22 13:58:32 -0400

    .
    .

---
+++ src/calculadora.js
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import './calculadora.css';
+//IMPORTAR BOOTSTRAP
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CalculadoraService from './calculadora.service';
+
+import {
+  Container, Row, Col, Button, Form
+} from 'react-bootstrap';
+
+
+
+
+
+function Calculadora() {
+
+  const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService(); // Instância
+
+  const [txtNumeros, setTxtNumeros] = useState('0');
+
+  const [numero1, setNumero1] = useState('0');
+
+  const [numero2, setNumero2] = useState(null);
+
+  const [operacao, setOperacao] = useState(null);
+
+  function adicionarNumero(numero) {
+    let resultado;
+
+    if (operacao === null) {
+      resultado = concatenarNumero(numero1, numero)
+      setNumero1(resultado);
+    } else {
+      resultado = concatenarNumero(numero2, numero)
+      setNumero2(resultado)
+    }
+
+    setTxtNumeros(resultado)
+  }
+
+  function definirOperacao(op) {
+    if (operacao === null) {
+      setOperacao(op);
+      return;
+    }
+
+    if (numero2 !== null) {
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+      setOperacao(op);
+      setNumero1(resultado.toString())
+      setNumero2(null)
+      setTxtNumeros(resultado.toString())
+    }
+  }
+
+  function acaoCalcular(){
+    if(numero2 === null){
+      return;
+    }
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+    setTxtNumeros(resultado.toString())
+  }
+
+  function limpar(){
+    setTxtNumeros('0')
+    setNumero1('0')
+    setNumero2(null)
+    setOperacao(null)
+  }
+
+
+
+  return (
+    <div class="mt-4 p-5 bg-primary text-white rounded">
+    <Container>
+        <Row>
+          <Col xs="3">
+            <Button variant="danger"
+            onClick={limpar}>C</Button>
+          </Col>
+          <Col xs="9">
+            <Form.Control type="text"
+              name="txtNumeros"
+              className="text-right"
+              readOnly="readOnly"
+              value={txtNumeros} 
+              data-testid="txtNumeros"/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('7')}>7</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('8')}>8</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('9')}>9</Button>
+          </Col>
+          <Col>
+            <Button variant="warning"
+              onClick={() => definirOperacao(DIVISAO)}>/</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('4')}>4</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('5')}>5</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('6')}>6</Button>
+          </Col>
+          <Col>
+            <Button variant="warning"
+              onClick={() => definirOperacao(MULTIPLICACAO)}>*</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('1')}>1</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('2')}>2</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('3')}>3</Button>
+          </Col>
+          <Col>
+            <Button variant="warning"
+              onClick={() => definirOperacao(SUBTRACAO)}>-</Button>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('0')}>0</Button>
+          </Col>
+          <Col>
+            <Button variant="light"
+              onClick={() => adicionarNumero('.')}>.</Button>
+          </Col>
+          <Col>
+            <Button variant="success"
+            onClick={acaoCalcular}>=</Button>
+          </Col>
+          <Col>
+            <Button variant="warning"
+              onClick={() => definirOperacao(SOMA)}>+</Button>
+          </Col>
+        </Row>
+      </Container>
+  </div>
+    );
+}
+
+export default Calculadora;
---
+++ src/calculadora.test.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Calculadora from './calculadora';
+
+test('renders learn react link', () => {
+  render(<Calculadora/>);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
